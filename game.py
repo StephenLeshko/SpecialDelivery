0ebd1195e9cc13c6c13ead48ea4be8d45a07b06f
StephenLeshko <stephen.leshko@gmail.com>
2022-07-05 15:40:23 -0400

    integrate voice control

--- game.py
+++ game.py
@@ -1,7 +1,11 @@
 import pygame 
 from sys import exit
 from random import randint, choice
+
+from pyparsing import Word
 import map_maker as mm
+import voice_control as vc
+import audioop
 
 
 pygame.init()
@@ -18,16 +22,66 @@ brick = pygame.image.load('images/brick.png').convert()
 #game objects & images
 car = pygame.image.load('images/car.png').convert_alpha()
 car_x, car_y = 45, 30
+car_vx, car_vy = 0, 0
 
 
 #important variables
-
+direction = 'down'
+moving = False
+directions = ['up', 'down', 'left', 'right']
 
 
 #functions
 
+
+def direction_choose():
+    global moving, direction
+    data = vc.stream.read(vc.FRAMES_PER_BUFFER)
+    rms = audioop.rms(data, 2)
+    if rms > 500 or vc.making_file == True:
+        vc.making_file = True
+        vc.frames.append(data)
+        vc.file_count += 1
+    if (vc.file_count == vc.RATE/vc.FRAMES_PER_BUFFER or rms < 150) and vc.making_file:
+        vc.file_count = 0
+        vc.create_audio(vc.frames)
+        word = vc.predict_audio('noise.wav')
+        print(word)
+        if word == 'go':
+            moving = True
+        elif word == 'stop':
+            moving = False
+        else:
+            direction = word
+        vc.kill_audio()
+        vc.frames.clear()
+        vc.making_file = False
+    
+
+
+
 def update():
-    pass
+    global car_x, car_y, car_vx, car_vy
+    direction_choose()
+    if moving:
+        if direction == 'right':
+            car_vx = 5
+            car_vy = 0
+        elif direction == 'left':
+            car_vx = -5
+            car_vy = 0
+        elif direction == 'up':
+            car_vx = 0
+            car_vy = -5
+        elif direction == 'down':
+            car_vx = 0
+            car_vy = 5
+    else:
+        car_vx = 0
+        car_vy = 0
+    car_x += car_vx
+    car_y += car_vy
+    
 
 #all the bliting takes place here
 def draw():
@@ -36,11 +90,14 @@ def draw():
     # screen.blit(brick, (0,0))
     # screen.blit(brick, (30, 0))
     # screen.blit()
-do = True
 
 
 mm.build_map(screen)
 game_bground = pygame.image.load('map.png').convert()
+
+#audio setup
+
+
 #---MAIN LOOP---
 while True:
     #event loop
@@ -55,6 +112,6 @@ while True:
     #     pygame.image.save(screen, 'dog.png')
     #     do = False
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(10)
     
 
---
+++ voice_control.py
@@ -0,0 +1,139 @@
+import os
+import pathlib
+import wave
+import pyaudio
+ 
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import tensorflow as tf
+import audioop
+ 
+from tensorflow.keras import layers
+from tensorflow.keras import models
+from IPython import display
+ 
+ 
+#setting basics for creating an audio file
+ 
+FRAMES_PER_BUFFER = 3200 #frames per second
+FORMAT = pyaudio.paInt16 #if stereo, make 32
+CHANNELS = 1 #if stereo, make 2
+RATE = 16000 #recording frequency
+ 
+ 
+ 
+#everything for the model
+ 
+commands = ['down', 'go', 'left', 'no', 'right', 'stop', 'up', 'yes']
+ 
+model = models.load_model('audioBasic1.h5')
+ 
+ 
+#turns audio into tensor
+def decode_audio(audio_binary):
+    # Decode WAV-encoded audio files to `float32` tensors, normalized
+    # to the [-1.0, 1.0] range. Return `float32` audio and a sample rate.
+    audio, _ = tf.audio.decode_wav(contents=audio_binary)
+    # Since all the data is single channel (mono), drop the `channels`
+    # axis from the array.
+    return tf.squeeze(audio, axis=-1)
+ 
+#turns wave file into spectrogram
+def get_spectrogram(waveform):
+    # Zero-padding for an audio waveform with less than 16,000 samples.
+    input_len = 16000
+    waveform = waveform[:input_len]
+    zero_padding = tf.zeros(
+        [16000] - tf.shape(waveform),
+        dtype=tf.float32)
+       
+    # Cast the waveform tensors' dtype to float32.
+    waveform = tf.cast(waveform, dtype=tf.float32)
+    # Concatenate the waveform with `zero_padding`, which ensures all audio
+    # clips are of the same length.
+    equal_length = tf.concat([waveform, zero_padding], 0)
+    # Convert the waveform to a spectrogram via a STFT.
+    spectrogram = tf.signal.stft(
+        equal_length, frame_length=255, frame_step=128)
+    # Obtain the magnitude of the STFT.
+    spectrogram = tf.abs(spectrogram)
+    # Add a `channels` dimension, so that the spectrogram can be used
+    # as image-like input data with convolution layers (which expect
+    # shape (`batch_size`, `height`, `width`, `channels`).
+    spectrogram = spectrogram[..., tf.newaxis]
+    return spectrogram
+ 
+#gets the file
+ 
+def predict_audio(file_name):
+    audio_binary = tf.io.read_file(file_name)
+    waveform = decode_audio(audio_binary)
+    spectrogram = get_spectrogram(waveform)
+    spectrogram = tf.reshape(spectrogram, [1, 124, 129, 1])
+    prediction = model(spectrogram)
+    return commands[int(tf.argmax(prediction, 1).numpy())]
+ 
+ 
+#idea is that the stream will continously run, and it will write data
+#once the audio reaches a certain volume, create a file, then run
+#the master audio function, then delete that file, then
+#reset what is in the loop
+#ends recording after 1 second, executes, and begins
+ 
+#----THE LOOP----
+# data = stream.read(CHUNK)
+# rms = audioop.rms(data, 2)
+pa = pyaudio.PyAudio()
+ 
+stream = pa.open(
+    format=FORMAT,
+    channels=CHANNELS,
+    rate=RATE,
+    input=True,
+    frames_per_buffer=FRAMES_PER_BUFFER
+)
+ 
+seconds = 10
+frames = []
+second_tracking = 0
+second_count = 0
+making_file = False
+file_count = 0
+file_max = 1
+ 
+#gets called once the making_file has returned to false
+def create_audio(frames):
+    obj = wave.open('noise.wav', 'wb')
+    obj.setnchannels(CHANNELS)
+    obj.setsampwidth(pa.get_sample_size(FORMAT))
+    obj.setframerate(RATE)
+    obj.writeframes(b''.join(frames))
+    obj.close()
+ 
+def kill_audio():
+    os.remove('noise.wav')
+ 
+ 
+# for i in range(0, int(RATE/FRAMES_PER_BUFFER*seconds)):
+#     data = stream.read(FRAMES_PER_BUFFER)
+#     rms = audioop.rms(data, 2)
+#     # print(rms)
+#     if rms > 500 or making_file == True:
+#         making_file = True
+#         frames.append(data)
+#         file_count += 1
+       
+#     if (file_count == RATE/FRAMES_PER_BUFFER or rms < 150) and making_file:
+#         file_count = 0
+#         create_audio(frames)
+#         print(predict_audio('noise.wav'))
+#         kill_audio()
+#         frames.clear()
+#         making_file = False
+#     second_tracking += 1
+#     if second_tracking == RATE/FRAMES_PER_BUFFER:
+#         second_count += 1
+#         second_tracking = 0
+#         print(f'Time Left: {seconds - second_count} seconds')
+
