2185de829d32926c6fa06e85deb6d10f6e21d71e
StephenLeshko <stephen.leshko@gmail.com>
2022-07-06 13:22:19 -0400

    coin collection and timer

--- game.py
+++ game.py
@@ -13,12 +13,14 @@ pygame.init()
 screen = pygame.display.set_mode((1200, 600))
 pygame.display.set_caption('Special Delivery')
 clock = pygame.time.Clock()
-# font = pygame.font.Font('font/Pixeltype.ttf', 50)
+
 #sounds
 
 #background images
 game_bground = pygame.image.load('images/game_bground.png').convert()
+load_screen = pygame.image.load('images/load_screen.png').convert()
 brick = pygame.image.load('images/brick.png').convert()
+
 
 #game objects & images
 car = pygame.image.load('images/car_1.png').convert_alpha()
@@ -32,15 +34,36 @@ car_5 = pygame.image.load('images/car_5.png').convert_alpha()
 
 cars = [car_1, car_2, car_3, car_4, car_5]
 car_index = 0.0
-car_vx, car_vy = 0, 5
+car_vx, car_vy = 0, 0
 car_rect = car.get_rect(topleft=(45, 30))
+score = 0
+
+#customer 
+customer = pygame.Surface((25, 25))
+customer.fill('Yellow')
+customer_rect = customer.get_rect(topleft=(1110, 510))
+
+#game text
+font_big = pygame.font.Font('freesansbold.ttf', 70)
+font_med = pygame.font.Font('freesansbold.ttf', 35)
+font_sma = pygame.font.Font('freesansbold.ttf', 25)
+
+space_begin = font_med.render('Press Space to Begin', True, (0, 255, 255))
+controls = font_sma.render('Voice Controls: Go, Stop, Right, Left, Up, Down', True, (100, 255, 255))
+score_hud = font_big.render(f'Score: {score}', True, (240, 240, 10))
+
+space_rect = space_begin.get_rect(center = (850, 450))
+controls_rect = controls.get_rect(center = (850, 500))
+second_counter = 120
+time_hud = font_med.render(f'Time: {second_counter}', True, (240, 8, 0))
+time_remain = 20
 
 
 #important variables
 direction = 'down'
 moving = False
 directions = ['up', 'down', 'left', 'right']
-
+game_active = False
 
 #functions
 def animate_cars():
@@ -51,11 +74,15 @@ def animate_cars():
     car = cars[int(car_index)]
 
 def check_collision():
+    
     if len(mm.rect_list) != 0:
         for rect in mm.rect_list:
             if pygame.Rect.colliderect(car_rect, rect):
+                print('collision')
                 return True
     return False
+
+
 
 def direction_choose():
     global moving, direction
@@ -86,10 +113,16 @@ def direction_choose():
         return True
     return False
     
-
+def drop_time():
+    global time_remain, second_counter
+    time_remain -= 1
+    if time_remain == 0:
+        time_remain = 20
+        second_counter -= 1
+        
 
 def update():
-    global car_vx, car_vy, car, car_rect
+    global car_vx, car_vy, car, car_rect, moving, time_hud
     if direction_choose():
         if moving:
             animate_cars()
@@ -109,27 +142,45 @@ def update():
                 car = pygame.transform.rotate(car, 180)
                 car_vx = 0
                 car_vy = 3.5
-        else:
-            car_vx = 0
-            car_vy = 0
+            else:
+                car_vx = 0
+                car_vy = 0
+    if moving == False:
+        car_vx = 0
+        car_vy = 0
+    #check collision
+    if check_collision():
+        car_rect.x -= (2 * car_vx)
+        car_rect.y -= (2 * car_vy)
+        moving = False
+        car_vx = 0
+        car_vy = 0
     car_rect.x += car_vx
     car_rect.y += car_vy
+    win()
+    drop_time()
+    time_hud = font_med.render(f'Time: {second_counter}', True, (240, 8, 0))
+
     
+def win():
+    if pygame.Rect.colliderect(car_rect, customer_rect):
+        coin = pygame.mixer.Sound('coin.wav')
+        coin.set_volume(1)
+        coin.play()
 
 #all the bliting takes place here
 def draw():
     screen.blit(game_bground, (0, 0))
     screen.blit(car, car_rect)
-    # screen.blit(brick, (0,0))
-    # screen.blit(brick, (30, 0))
-    # screen.blit()
+    screen.blit(customer, customer_rect)
+    screen.blit(time_hud, (1000, 40))
 
 
 mm.build_map(screen)
 game_bground = pygame.image.load('map.png').convert()
-# bg_music = pygame.mixer.Sound('theme.wav')
-# bg_music.set_volume(0.2)
-# bg_music.play(loops=-1)
+bg_music = pygame.mixer.Sound('theme.wav')
+bg_music.set_volume(0.2)
+bg_music.play(loops=-1)
 #audio setup
 
 
@@ -140,13 +191,21 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        if game_active == False:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
     #draw & update cycle
-    update()
-    draw()
+    if game_active:
+        update()
+        draw()
+    else:
+        screen.blit(load_screen, (0, 0))
+        screen.blit(space_begin, space_rect)
+        screen.blit(score_hud, (550, 200))
+        screen.blit(controls, controls_rect)
     # if do:
     #     pygame.image.save(screen, 'dog.png')
     #     do = False
     pygame.display.update()
-    clock.tick(60)
-    
-
+    #usually at 60... what does it actually run at
+    clock.tick(20) #4.37 seconds at 90
